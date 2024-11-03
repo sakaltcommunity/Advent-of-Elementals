e3547768a0b91a8d3c4e2af4d8af3031daf177f3
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-11-03 17:25:36 +0900

    Update AOE.java

--- src/main/java/com/sakalti/AOE.java
+++ src/main/java/com/sakalti/AOE.java
@@ -1,15 +1,16 @@
 package com.sakalti.aoe;
 
-import com.sakalti.aoe.entities.Shelran;
 import com.sakalti.aoe.items.Amethyst;
-import com.sakalti.aoe.world.RuboreomWorld;
+import com.sakalti.aoe.items.CoralSword;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.model.ModelResourceLocation;
 
@@ -18,18 +19,13 @@ public class AOE {
     public static final String MODID = "aoe";
 
     public AOE() {
-        // イベントリスナーの登録
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
-        // アメジストアイテムの登録
         Amethyst.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        // ルボレオムのワールドをセットアップ
-        RuboreomWorld.setup();
-        // シェランエンティティの登録
-        Shelran.SHELRAN.register(FMLJavaModLoadingContext.get().getModEventBus());
+        CoralSword.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus()); // コーラルソードを登録
     }
 
     private void doClientStuff(final FMLClientSetupEvent event) {
@@ -43,6 +39,8 @@ public class AOE {
             new ModelResourceLocation(AOE.MODID + ":amethyst_sword", "inventory"));
         Minecraft.getInstance().getItemRenderer().getItemModelShaper().register(Amethyst.AMETHYST_AXE.get(), 
             new ModelResourceLocation(AOE.MODID + ":amethyst_axe", "inventory"));
+        Minecraft.getInstance().getItemRenderer().getItemModelShaper().register(CoralSword.CORAL_SWORD.get(), 
+            new ModelResourceLocation(AOE.MODID + ":coral_sword", "inventory"));
     }
 
     @SubscribeEvent
@@ -50,6 +48,7 @@ public class AOE {
         if (event.getTab() == CreativeModeTabs.COMBAT) {
             event.accept(Amethyst.AMETHYST_SWORD);
             event.accept(Amethyst.AMETHYST_AXE);
+            event.accept(CoralSword.CORAL_SWORD); // コーラルソードを戦闘タブに追加
         }
     }
 }
